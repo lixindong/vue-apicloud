e84da6782f2878032413c2b5ee42020e7ae2e923
Chan <958501820@qq.com>
2017-12-15 16:51:33 +0800

    kdkd

---
+++ src/components/foot/foot.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="foot">
+            childMsg
+            <span>
+                {{clide}}
+            </span>
+    </div>
+     </template>
+
+     <script>
+      export default {
+        name: 'foot',
+        data () {
+          return {
+            childMsg: 'Welcome to Your Vue.js App',
+            clide:this.message
+          }
+        },
+        props: ['message']
+
+      }
+    </script>
+
+    <!-- Add "scoped" attribute to limit CSS to this component only -->
+    <style scoped>
+          .footer .about, .footer .blog, .footer .follow, .footer .contact {
+      width: 32%;
+    }
+    </style>
--- src/pages/html/index/app.js
+++ src/pages/html/index/app.js
@@ -1,12 +1,16 @@
 import App from './app.vue';
 import Vue from 'vue';
 import vTap from 'v-tap';
+import  'public/api'
+import 'assets/css/aui.css';
+
 Vue.use(vTap); //添加vue tap事件
-
-
+console.log(window.$api)
 window.apiready = function() {
     new Vue({
         el: '#app',
         render: h => h(App)
     })
+    let header = document.getElementById('header');
+    window.$api.fixStatusBar(header);
 }
